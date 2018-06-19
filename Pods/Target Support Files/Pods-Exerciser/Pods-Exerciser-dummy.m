3f583dfeb9fe42139e8a0aebb51fd9fa4a6c8d20
Markus Kruusmägi <markus.kruusmagi@isotop.se>
2018-06-19 12:29:33 +0200

    Fix folder structure

---
+++ Pods/Target Support Files/Pods-Exerciser/Pods-Exerciser-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Exerciser : NSObject
+@end
+@implementation PodsDummy_Pods_Exerciser
+@end
---
+++ Pods/Target Support Files/Pods-ExerciserTests/Pods-ExerciserTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ExerciserTests : NSObject
+@end
+@implementation PodsDummy_Pods_ExerciserTests
+@end
